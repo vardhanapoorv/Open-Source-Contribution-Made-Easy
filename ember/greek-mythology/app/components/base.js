f0714cc4c574686bf3ee456bf08d8cbfcf0b6913
Apoorv Vardhan <vardhanapoorv@gmail.com>
2019-09-21 23:06:29 +0530

    Ember version of basic Greek Mythology app :star:

---
+++ ember/greek-mythology/app/components/base.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Apoorv Vardhan 
+ * @Date: 2019-09-21 22:46:08 
+ * @Last Modified by: Apoorv Vardhan
+ * @Last Modified time: 2019-09-21 23:01:08
+ */
+import Component from '@ember/component';
+
+const GreekGods = [
+    {
+      "name": "Zeus",
+      "desc": "Zeus is the sky and thunder god in ancient Greek religion, who rules as king of the gods of Mount Olympus"
+    },
+    {
+      "name": "Ares",
+      "desc": "Ares is the Greek god of war. He is one of the Twelve Olympians, the son of Zeus and Hera. In Greek literature, he often represents the physical or violent and untamed aspect of war"
+    }
+  ];
+
+export default Component.extend({
+    gods: GreekGods
+});
---
+++ ember/greek-mythology/app/components/card.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Apoorv Vardhan 
+ * @Date: 2019-09-21 22:46:12 
+ * @Last Modified by:   Apoorv Vardhan 
+ * @Last Modified time: 2019-09-21 22:46:12 
+ */
+import Component from '@ember/component';
+
+export default Component.extend({
+});
