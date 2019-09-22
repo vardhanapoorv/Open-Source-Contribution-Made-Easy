1b286ce152086921d71ff5f269340532a0114817
Apoorv Vardhan <vardhanapoorv@gmail.com>
2019-09-22 23:54:39 +0530

    Node basic app created with GraphQl setup using Apollo-Server to be done :thought_balloon:

--- node/greek-mythology/appwithgraphqlusingapollo/app.js
+++ node/greek-mythology/appwithgraphqlusingapollo/app.js
@@ -1,6 +1,22 @@
 /*
  * @Author: Apoorv Vardhan 
  * @Date: 2019-09-22 23:25:03 
- * @Last Modified by:   Apoorv Vardhan 
- * @Last Modified time: 2019-09-22 23:25:03 
+ * @Last Modified by: Apoorv Vardhan
+ * @Last Modified time: 2019-09-22 23:46:04
  */
+
+/*
+TODO: Create the GraphQL setup using Apollo-Server. Refer Apollo Docs for help. 
+*/
+
+const mongoose = require('mongoose');
+
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@oss-contri-jmuc0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+, {useNewUrlParser: true}).then(() => {
+    app.listen(8000);
+}).catch(err => {
+    console.log(err);
+})
