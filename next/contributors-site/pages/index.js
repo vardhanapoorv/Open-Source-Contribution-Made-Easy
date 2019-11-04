6a8d5fb0082f99609001e11fd084a356302c5af7
Emizel <Ernestogl5@hotmail.com>
2019-11-04 11:04:19 -0400

    Add contributors cards"

--- next/contributors-site/pages/index.js
+++ next/contributors-site/pages/index.js
@@ -1,29 +1,11 @@
-import Layout from "../components/Layout";
-import { FaTwitter, FaGithub } from "react-icons/fa";
-
-import Contributors from "../static/contributors.json";
+import Layout from '../components/Layout';
+import Cards from '../components/Cards';
 
 export default function Index() {
-  return (
-    <Layout>
-      <h2>List of Contributors</h2>
-      <ol>
-        {Contributors.map((contributor, index) => {
-          return (
-            <li key={index}>
-              <span style={{ fontSize: "large" }}>
-                {contributor.name}
-                <a href={contributor.github} target="_blank">
-                  <FaGithub color="#211F1F" />
-                </a>
-                <a href={contributor.twitter} target="_blank">
-                  <FaTwitter color="#00acee" />
-                </a>
-              </span>
-            </li>
-          );
-        })}
-      </ol>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h2>List of Contributors</h2>
+            <Cards />
+        </Layout>
+    );
 }
